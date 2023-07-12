7e92afa8c6c375097fddc7e566000ed7ec8e51e9
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-07-12 14:37:49 +0300

    backend security oluşturuldu.

--- api/server.js
+++ api/server.js
@@ -3,8 +3,14 @@
 const express = require("express");
 const server = express();
 require("dotenv").config();
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 
 // 2.global middleware'larım
+server.use(helmet());
+server.use(cors());
+server.use(morgan("dev"));
 
 // 3.routerlarım
 server.get("/", (req, res) => {
