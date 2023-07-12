5aab6320f23d87ce060e9fa50603152058913533
kubrasertkaya <118736856+kubrasertkaya@users.noreply.github.com>
2023-07-12 10:41:41 +0300

    proje folder yapısı oluşturuluyor.

---
+++ api/server.js
@@ -0,0 +1,17 @@
+// 1.importlarım
+
+const express = require("express");
+const server = express();
+
+// 2.global middleware'larım
+
+// 3.routerlarım
+server.get("/", (req, res) => {
+  res.json({ message: "Server up and running...." }); //test amaçlı bir endpoint yazdık.
+});
+
+// 4.ERROR middleware
+
+// 5.export
+
+module.exports = server;
